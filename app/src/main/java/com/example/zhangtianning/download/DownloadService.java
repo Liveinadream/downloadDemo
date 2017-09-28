326233513bc18cf7fae96199390d8e70bb826809
张天宁 <zhangtianning@bjknrt.com>
2017-09-28 17:15:34 +0800

    能够正常的使用downloadManager下载文件并打开，暂时只能下载一个文件，否则数据库信息可能会出现问题，参考简书作者Phoenix， 添加了一些监听，暂未放置在有效代码中，下载功能已经实现，但是代码有待整理。

--- app/src/main/java/com/example/zhangtianning/download/DownloadService.java
+++ app/src/main/java/com/example/zhangtianning/download/DownloadService.java
@@ -1,42 +1,31 @@
 package com.example.zhangtianning.download;
 
 import android.app.DownloadManager;
-import android.app.IntentService;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
+import android.os.Binder;
 import android.os.Environment;
 import android.os.IBinder;
 import android.os.Message;
-import android.os.Parcelable;
-import android.os.ResultReceiver;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
-import android.widget.Toast;
 
 import com.example.zhangtianning.download.dao.DownloadFileInfo;
 import com.example.zhangtianning.download.daoutils.DBDaoImpl;
-import com.example.zhangtianning.download.listener.FileDownloadListener;
 import com.example.zhangtianning.download.utils.FileUtils;
 import com.example.zhangtianning.download.utils.LogUtils;
-import com.example.zhangtianning.download.utils.OpenApk;
+import com.example.zhangtianning.download.utils.WeakHandler;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zhangtianning on 2017/9/11
@@ -45,6 +34,8 @@ import java.net.URLConnection;
 public class DownloadService extends Service {
 
     private static final String TAG = DownloadService.class.getSimpleName();
+
+
     /**
      * 文件下载目录
      */
@@ -56,7 +47,10 @@ public class DownloadService extends Service {
     private static final String FILE_DOWNLOAD_TEMP_DIR = "/knrt/temp/";
 
     private DownloadManager dm;
-    private long enqueue;
+    private DownloadChangeObserver downloadObserver;
+    private BroadcastReceiver downLoadBroadcast;
+    private ScheduledExecutorService scheduledExecutorService;
+    private long requestId;
     private int requestCode = (int) SystemClock.uptimeMillis();
     private BroadcastReceiver receiver;
     //    private String url = "http://d.koudai.com/com.koudai.weishop/1000f/weishop_1000f.apk";
@@ -66,6 +60,18 @@ public class DownloadService extends Service {
     DownloadFileInfo downloadFileInfo;//文件信息
     private File tmpFile;
     Context context;
+    private DownloadBinder binder;
+    public static final int STATE_START_DOWNLOAD = 0; // 开始下载
+    public static final int STATE_DOWNLOADING = 1;   //  下载中
+    public static final int STATE_FINISH_DOWNLOAD = 2;  // 下载完成
+    public static final int STATE_FAIL_DOWNLOAD = 3; // 下载失败
+    public static final int STATE_FAIL_DOWNLOAD_PAUSE = 4; // 下载暂停
+
+    public int downloadState = -1; //最初的下载状态，没有下载任务。
+    public MyWeakHandler myWeakHandler;
+    private OnProgressListener onProgressListener;
+    public static final int HANDLE_DOWNLOAD = 0x001;
+
 
 
     @Nullable
@@ -75,10 +81,16 @@ public class DownloadService extends Service {
         return null;
     }
 
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
-        url = intent.getStringExtra(URL);
+        myWeakHandler = new MyWeakHandler(this);
+        if (intent != null) {
+            url = intent.getStringExtra(URL);
+        } else {
+            LogUtils.E(TAG, "没有传递内容");
+            return Service.START_NOT_STICKY;
+        }
         context = this;
 
         downloadFileInfo = DBDaoImpl.getInstance(context).getDownloadFileInfoWithUrl(url);
@@ -86,10 +98,13 @@ public class DownloadService extends Service {
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
+                LogUtils.D(TAG, "receiver 的情况:path " + downloadFileInfo.getFilePath());
+                setDownloadInfo(requestId);
+//                getDownloadQuery(requestId);
+
                 intent = new Intent(Intent.ACTION_VIEW);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory()
-                                + "//knrt/temp/myApp.apk")),
+                intent.setDataAndType(Uri.fromFile(new File(downloadFileInfo.getFilePath())),
                         "application/vnd.android.package-archive");
                 startActivity(intent);
                 stopSelf();
@@ -97,19 +112,12 @@ public class DownloadService extends Service {
         };
 
         registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-
         startDownload();
         return Service.START_STICKY;
     }
 
-    @Override
-    public void onDestroy() {
-        unregisterReceiver(receiver);
-        super.onDestroy();
-    }
 
     private void startDownload() {
-        String basePath = FileUtils.createBasePath(FILE_DOWNLOAD_DIR);
         String tmpfileBasePath = FileUtils.createBasePath(FILE_DOWNLOAD_TEMP_DIR);
 
 
@@ -130,13 +138,20 @@ public class DownloadService extends Service {
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
 
         request.setMimeType("application/vnd.android.package-archive");
-//        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "myApp.apk");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 
+        if (fileName == null) {
+            fileName = url.substring(url.lastIndexOf("/") + 1);
+            downloadFileInfo.setFileName(fileName);
+
+        }
+
+        request.setDestinationInExternalPublicDir(FILE_DOWNLOAD_DIR,
+                downloadFileInfo.getFileName());
 
         if (downloadSize > 0) {
             String tmpFilePath = Environment.getExternalStorageDirectory().getPath() + FILE_DOWNLOAD_TEMP_DIR + fileName;
             LogUtils.D("Tag", "临时文件的路径：" + tmpFilePath);
-            fileLength = downloadFileInfo.getFileSize();
             request.addRequestHeader("Range",
                     "bytes=" + downloadFileInfo.getHadDownloadSize() + "-"
                             + downloadFileInfo.getFileSize());
@@ -145,10 +160,11 @@ public class DownloadService extends Service {
                 tmpFile = FileUtils.createTempFile(tmpfileBasePath, fileName);
                 downloadFileInfo.setTempFilePath(tmpFile.getAbsolutePath());
             }
-            enqueue = dm.enqueue(request);
+            requestId = dm.enqueue(request);
         } else {
-            enqueue = dm.enqueue(request);
-            DownloadManager.Query query = new DownloadManager.Query().setFilterById(enqueue);
+            requestId = dm.enqueue(request);
+
+            DownloadManager.Query query = new DownloadManager.Query().setFilterById(requestId);
             Cursor cursor = null;
 
             try {
@@ -157,17 +173,7 @@ public class DownloadService extends Service {
 
                     //下载文件的总大小
                     fileLength = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
-
-                    //已经下载文件大小
-//                    bytesAndStatus[0] = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
-//
-//                    //下载状态
-//                    bytesAndStatus[2] = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
-
-                    if (fileName == null) {
-                        String path = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME)); // 获取文件路径
-                        fileName = path.substring(path.lastIndexOf("/") + 1, path.length());
-                    }
+                    downloadFileInfo.setFileSize(fileLength);
                 }
 
 
@@ -176,23 +182,218 @@ public class DownloadService extends Service {
                     cursor.close();
                 }
             }
-            downloadFileInfo.setFileSize(fileLength);
 
-            if (fileName == null) {
-                fileName = url.substring(url.lastIndexOf("/") + 1);
-            }
-            downloadFileInfo.setFileName(fileName);
-            downloadFileInfo.setFilePath(basePath + File.separator + fileName);
             String tmpFilePath = Environment.getExternalStorageDirectory().getPath() + FILE_DOWNLOAD_TEMP_DIR + fileName;
-            LogUtils.D("Tag", "临时文件的路径：" + tmpFilePath);
             tmpFile = FileUtils.createTempFile(tmpfileBasePath, fileName);
             downloadFileInfo.setTempFilePath(tmpFile.getAbsolutePath());
             DBDaoImpl.getInstance(context).Updata(downloadFileInfo);
 
-            request.setDestinationInExternalPublicDir(FileUtils.createBasePath(FILE_DOWNLOAD_DIR),
-                    downloadFileInfo.getFileName());
+
         }
 
     }
 
+
+    private void getDownloadQuery(long requestId) {
+        DownloadManager.Query query = new DownloadManager.Query();
+        Cursor cursor = dm.query(query.setFilterById(requestId));
+        if (cursor != null && cursor.moveToFirst()) {
+            String path = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME)); // 获取文件路径
+            LogUtils.D(TAG, "路径：" + path);
+        }
+
+    }
+
+    private void setDownloadInfo(long requestId) {
+        DownloadManager.Query query = new DownloadManager.Query();
+        Cursor cursor = dm.query(query.setFilterById(requestId));
+        if (cursor != null && cursor.moveToFirst()) {
+            //下载的文件到本地的目录
+//            String address = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+            //已经下载的字节数
+            int bytes_downloaded = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+
+
+            downloadFileInfo.setHadDownloadSize((long) bytes_downloaded);
+
+//            总需下载的字节数
+            int bytes_total = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+            downloadFileInfo.setFileSize((long) bytes_total);
+
+            //Notification 标题
+            String title = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_TITLE));
+            LogUtils.D(TAG, "标题:" + title);
+            //描述
+            String description = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_DESCRIPTION));
+            LogUtils.D(TAG, "描述:" + description);
+
+            //下载对应id
+            long id = cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_ID));
+            //下载文件名称
+            String path = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME));
+
+            downloadFileInfo.setFilePath(path);
+            LogUtils.D(TAG, "文件名:" + path);
+
+
+            //下载文件的URL链接
+            String url = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_URI));
+        }
+        DBDaoImpl.getInstance(context).Updata(downloadFileInfo);
+
+    }
+
+    public class DownloadBinder extends Binder {
+        /**
+         * 返回当前服务的实例
+         *
+         * @return
+         */
+        public DownloadService getService() {
+            return DownloadService.this;
+        }
+
+    }
+
+    /**
+     * 注销广播
+     */
+    private void unregisterBroadcast() {
+        if (downLoadBroadcast != null) {
+            unregisterReceiver(downLoadBroadcast);
+            downLoadBroadcast = null;
+        }
+    }
+
+
+    /**
+     * 注销ContentObserver
+     */
+    private void unregisterContentObserver() {
+        if (downloadObserver != null) {
+            getContentResolver().unregisterContentObserver(downloadObserver);
+        }
+    }
+
+    /**
+     * 注册ContentObserver
+     */
+    private void registerContentObserver() {
+        /** observer download change **/
+        if (downloadObserver != null) {
+            getContentResolver().registerContentObserver(Uri.parse("content://downloads/my_downloads"), false, downloadObserver);
+        }
+    }
+
+    public interface OnProgressListener {
+        /**
+         * 下载进度
+         *
+         * @param fraction 已下载/总大小
+         */
+        void onProgress(float fraction);
+    }
+
+    public void setOnProgressListener(OnProgressListener onProgressListener) {
+        this.onProgressListener = onProgressListener;
+    }
+
+
+    /**
+     * 监听下载进度
+     */
+    private class DownloadChangeObserver extends ContentObserver {
+
+        public DownloadChangeObserver() {
+            super(myWeakHandler);
+            scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        }
+
+        /**
+         * 当所监听的Uri发生改变时，就会回调此方法
+         *
+         * @param selfChange 此值意义不大, 一般情况下该回调值false
+         */
+        @Override
+        public void onChange(boolean selfChange) {
+            scheduledExecutorService.scheduleAtFixedRate(progressRunnable, 0, 2, TimeUnit.SECONDS);
+        }
+    }
+
+    private static final class MyWeakHandler extends WeakHandler<DownloadService> {
+
+        MyWeakHandler(DownloadService downloadService) {
+            super(downloadService);
+        }
+
+        @Override
+        public void handleMessage(Message msg, DownloadService downloadService) {
+            if (downloadService == null) {
+                return;
+            }
+            if (downloadService.onProgressListener != null && HANDLE_DOWNLOAD == msg.what) {
+                //被除数可以为0，除数必须大于0
+                if (msg.arg1 >= 0 && msg.arg2 > 0) {
+                    downloadService.onProgressListener.onProgress(msg.arg1 / (float) msg.arg2);
+                }
+            }
+        }
+    }
+
+    private Runnable progressRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateProgress();
+        }
+    };
+
+
+    /**
+     * 发送Handler消息更新进度和状态
+     */
+    private void updateProgress() {
+        int[] bytesAndStatus = getBytesAndStatus(requestId);
+        myWeakHandler.sendMessage(myWeakHandler.obtainMessage(HANDLE_DOWNLOAD, bytesAndStatus[0], bytesAndStatus[1], bytesAndStatus[2]));
+    }
+
+
+    /**
+     * 通过query查询下载状态，包括已下载数据大小，总大小，下载状态
+     *
+     * @param downloadId
+     * @return
+     */
+    private int[] getBytesAndStatus(long downloadId) {
+        int[] bytesAndStatus = new int[]{
+                -1, -1, 0
+        };
+        DownloadManager.Query query = new DownloadManager.Query().setFilterById(downloadId);
+        Cursor cursor = null;
+        try {
+            cursor = dm.query(query);
+            if (cursor != null && cursor.moveToFirst()) {
+                //已经下载文件大小
+                bytesAndStatus[0] = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                //下载文件的总大小
+                bytesAndStatus[1] = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+                //下载状态
+                bytesAndStatus[2] = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return bytesAndStatus;
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(receiver);
+        unregisterBroadcast();
+        unregisterContentObserver();
+        super.onDestroy();
+    }
 }
--- app/src/main/java/com/example/zhangtianning/download/UpdataActivity.java
+++ app/src/main/java/com/example/zhangtianning/download/UpdataActivity.java
@@ -1,10 +1,18 @@
 package com.example.zhangtianning.download;
 
+import android.app.DownloadManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.ContentObserver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -14,20 +22,33 @@ import android.widget.Toast;
 
 import com.example.zhangtianning.download.dao.DownloadFileInfo;
 import com.example.zhangtianning.download.daoutils.DBDaoImpl;
+import com.example.zhangtianning.download.listener.FileDownloadListener;
 import com.example.zhangtianning.download.utils.LogUtils;
 import com.example.zhangtianning.download.utils.OpenApk;
 import com.example.zhangtianning.download.utils.ToastUtils;
+import com.example.zhangtianning.download.utils.WeakHandler;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.logging.Logger;
 
 
 /**
  * Created by 冒险者ztn on 2017/9/15.
  */
 
-public class UpdataActivity extends AppCompatActivity {
+public class UpdataActivity extends AppCompatActivity implements FileDownloadListener {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    public static final int STATE_START_DOWNLOAD = 0; // 开始下载
+    public static final int STATE_DOWNLOADING = 1;   //  下载中
+    public static final int STATE_FINISH_DOWNLOAD = 2;  // 下载完成
+    public static final int STATE_FAIL_DOWNLOAD = 3; // 下载失败
+    public static final int STATE_FAIL_DOWNLOAD_PAUSE = 4; // 下载暂停
+
+    public int downloadState = -1; //最初的下载状态，没有下载任务。
+
 
     //目前可能需要的参数，用户名，token，
 
@@ -36,11 +57,17 @@ public class UpdataActivity extends AppCompatActivity {
     private Button jump, parse, start;
     UpdataActivity updataActivity;
     private int versionCode;
+    private long requestId = -1;
 
     private static final String DOWNLOADFILEINFO = "downloadFileInfo";
     DownloadFileInfo downloadFileInfo;//文件信息
     private String downloadUrl = "http://d.koudai.com/com.koudai.weishop/1000f/weishop_1000f.apk";
     private static String URL = "url";
+    DownloadManager downloadManager;
+
+
+
+
 
 
     @Override
@@ -79,11 +106,44 @@ public class UpdataActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isUrlInDB(updataActivity, downloadUrl);
+                if (downloadState == STATE_DOWNLOADING) {
+                    Toast.makeText(updataActivity, "有文件正在下载", Toast.LENGTH_SHORT).show();
+                } else {
+                    isUrlInDB(updataActivity, downloadUrl);
+                }
             }
         });
     }
 
+    private ServiceConnection conn = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            DownloadService.DownloadBinder binder = (DownloadService.DownloadBinder) service;
+            DownloadService downloadService = binder.getService();
+
+            //接口回调，下载进度
+            downloadService.setOnProgressListener(new DownloadService.OnProgressListener() {
+                @Override
+                public void onProgress(float fraction) {
+//                    Log.i(TAG, "下载进度：" + fraction);
+//                    bnp.setProgress((int)(fraction * 100));
+//
+//                    //判断是否真的下载完成进行安装了，以及是否注册绑定过服务
+//                    if (fraction == DownloadService.UNBIND_SERVICE && isBindService) {
+//                        unbindService(conn);
+//                        isBindService = false;
+//                        MToast.shortToast("下载完成！");
+//                    }
+                }
+            });
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     /**
      * 这个地方由于downloadManger要求http或者https开头的所以取消了www开头的url
@@ -99,7 +159,8 @@ public class UpdataActivity extends AppCompatActivity {
             downloadFileInfo = DBDaoImpl.getInstance(context).getDownloadFileInfoWithUrl(url);
 
             if (downloadFileInfo != null) {
-                if (TextUtils.equals(downloadFileInfo.getHadDownloadSize().toString(), downloadFileInfo.getFileSize().toString())) {
+                if (TextUtils.equals(downloadFileInfo.getHadDownloadSize().toString(), downloadFileInfo.getFileSize().toString()) &&
+                        !TextUtils.equals("-1", downloadFileInfo.getFileSize().toString())) {
                     File file = new File(downloadFileInfo.getFilePath());
                     if (file.exists()) {
                         OpenApk.getInstance().openFile(updataActivity, file);
@@ -124,11 +185,32 @@ public class UpdataActivity extends AppCompatActivity {
     }
 
     private void startServiceDownload() {
+        downloadState = STATE_START_DOWNLOAD;
         Intent intent = new Intent(UpdataActivity.this, DownloadService.class);
-
         intent.putExtra(URL, downloadUrl);
         startService(intent);
     }
+
+    @Override
+    public void onFileDownloading(DownloadFileInfo downloadFileInfo) {
+        downloadState = STATE_DOWNLOADING;
+    }
+
+    @Override
+    public void onFileDownloadFail(DownloadFileInfo downloadFileInfo) {
+        downloadState = STATE_FAIL_DOWNLOAD;
+    }
+
+    @Override
+    public void onFileDownloadCompleted(DownloadFileInfo downloadFileInfo) {
+        downloadState = STATE_FINISH_DOWNLOAD;
+    }
+
+    @Override
+    public void onFileDownloadPaused(DownloadFileInfo downloadFileInfo) {
+        downloadState = STATE_FAIL_DOWNLOAD_PAUSE;
+    }
+
 
     @Override
     protected void onDestroy() {
